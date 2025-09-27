8a7cf4fb8cd3455246316aae532961e3603efd03
Forrest Weigel <eldrxofficial@gmail.com>
2025-09-27 14:08:07 -0500

    Intial packages for auth0 also logo files for auth0 and placeholder page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center p-8">
-            <Image src="/logo-full.svg" alt="Ikthus Logo" width={450} height={200} />
+            <Image src="/logo-full.png" alt="Ikthus Logo" width={450} height={200} />
         </div>
     )
 }
